f8d80eff33be2d97a3251a3de3df81b6e18d6407
djaumandreuonda <diego.jaumandreuonda@ada.ac.uk>
2023-05-21 17:11:54 +0100

    Update paymentprocessor.py

--- paymentprocessor.py
+++ paymentprocessor.py
@@ -1,1 +1,16 @@
-print("Hello from EC2")
+import rpyc, json, uuid
+from rpyc.utils.server import ThreadedServer
+
+class PaymentProcessor(rpyc.Service):
+  def exposed_process_payment(self, payment_info):
+      card_num = payment_info["card_num"].replace(" ", "") if "card_num" in payment_info else ""
+      if card_num.startswith("8002"):
+        print("Bisa card")
+
+      else:
+        print("NOT KNOWN")
+
+if __name__ == '__main__':
+    ts = ThreadedServer(PaymentProcessor,port=18080)
+    print('Service started on port 18080')
+    ts.start()
